1043a545edce6683e82b0d7e3125556b6b37f7a8
Jan <jdolkowski@soldevelo.com>
2024-01-08 15:12:50 +0100

    CM-405: allowa only one pending task (#29)
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>

--- tasks_management/validation.py
+++ tasks_management/validation.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
+from django.db.models import Q
+from django.contrib.contenttypes.models import ContentType
 
 from core.models import User
 from core.validation import BaseModelValidation
@@ -40,8 +42,28 @@ class TaskValidation(BaseModelValidation):
     OBJECT_TYPE = Task
 
     @classmethod
+    def validate_create(cls, user, **data):
+        errors = [
+            *validate_existing_task(data)
+        ]
+        if errors:
+            raise ValidationError(errors)
+        super().validate_create(user, **data)
+
+    @classmethod
     def validate_update(cls, user, **data):
-        errors = validate_task(data)
+        errors = [
+            *validate_task_status(data)
+        ]
+        if errors:
+            raise ValidationError(errors)
+        super().validate_create(user, **data)
+
+    @classmethod
+    def validate_delete(cls, user, **data):
+        errors = [
+            *validate_existing_task(data)
+        ]
         if errors:
             raise ValidationError(errors)
         super().validate_create(user, **data)
@@ -51,12 +73,6 @@ def validate_task_group(data, uuid=None):
     return [
         *validate_not_empty_field(data.get("code"), "code"),
         *validate_bf_unique_code(data.get('code'), uuid),
-    ]
-
-
-def validate_task(data):
-    return [
-        *validate_task_status(data)
     ]
 
 
@@ -98,5 +114,29 @@ def validate_not_empty_field(string, field):
     if not string:
         return [{"message": _("tasks_management.validation.field_empty") % {
             'field': field
+        }}]
+    return []
+
+
+def validate_existing_task(data):
+    content_type = data.get('entity_type')
+    entity_id = data.get('entity_id')
+
+    try:
+        entity_instance = content_type.get_object_for_this_type(id=entity_id)
+    except content_type.model_class().DoesNotExist:
+        return [{"message": _("tasks_management.validation.entity_not_found") % {
+            'entity_id': entity_id
+        }}]
+
+    filtered_tasks = Task.objects.filter(
+        Q(entity_type=content_type) &
+        Q(entity_id=str(entity_id)) &
+        (Q(status=Task.Status.ACCEPTED) | Q(status=Task.Status.RECEIVED))
+    )
+
+    if filtered_tasks.exists():
+        return [{"message": _("tasks_management.validation.another_task_pending") % {
+            'instance': str(entity_instance)
         }}]
     return []
