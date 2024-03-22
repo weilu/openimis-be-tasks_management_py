cc0963cbab21a6904de5bbcdf73540f3c793748e
Jan <jdolkowski@soldevelo.com>
2024-03-22 10:01:41 +0100

    CM-750: filter by entity string (#46)
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>

--- tasks_management/schema.py
+++ tasks_management/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import json
 import graphene_django_optimizer as gql_optimizer
 
 from django.contrib.auth.models import AnonymousUser
@@ -42,6 +43,7 @@ class Query(graphene.ObjectType):
         customFilters=graphene.List(of_type=graphene.String),
         taskGroupId=graphene.String(),
         entityIds=graphene.List(graphene.UUID),
+        entityString__Icontains=graphene.String(),
     )
 
     def resolve_task(self, info, **kwargs):
@@ -61,6 +63,12 @@ class Query(graphene.ObjectType):
 
         # not checking perms because get_queryset filters tasks assigned to user
         query = Task.objects.filter(*filters)
+
+        entity_string = kwargs.get("entityString__Icontains")
+        if entity_string:
+            task_ids = [task.id for task in query if entity_string.lower() in str(task.entity).lower()]
+            query = query.filter(id__in=task_ids)
+
         return gql_optimizer.query(query, info)
 
     def resolve_task_group(self, info, **kwargs):
